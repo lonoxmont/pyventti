b6bf77f77bb02c254748c2625883caf558ddc10b
lonoxmont <lonoxmont@suptg.com>
2025-06-14 16:28:27 -0700

    made some more progress on the game logic, it wont work as-is and i am too stupid to figure out why. probably gonna abandon this lets be real lmao

--- game.py
+++ game.py
@@ -1,3 +1,30 @@
 from hand import Hand
 from deck import Deck
 
+def main(self, deck):
+    self.deck = deck
+    PlayerHand = Hand
+    DealerHand = Hand
+    Deck.shuffle()
+
+    PlayerHand.draw_card()
+    PlayerHand.draw_card()
+    DealerHand.draw_card()
+
+    print(f"your hand is {PlayerHand.__repr__}")
+    print("would you like to (h)it or (s)tand?")
+    choice = input()
+    if len(choice) != 1 or choice.lower() != 'h' and choice.lower() != 's' :
+        print("Perkele! Pick an option from the list!")
+    
+    if choice == "h":
+        PlayerHand.draw_card()
+        
+            
+    if choice == "s":
+        pass
+
+
+
+
+main(Deck)
